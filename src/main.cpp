5c7feabfb0ad831129132eebf06a31ed3a85c313
ZackDelsnove <96685170+ZackDelsnova@users.noreply.github.com>
2026-02-14 16:03:05 +0530

    removed unused stuff

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,6 @@ struct WindowInfo {
     HWND hwnd;
     std::string title;
     RECT rect;
-    // LONG_PTR style;
-    // LONG_PTR exStyle;
     LayoutState state;
 };
 
@@ -35,67 +33,6 @@ auto FindManagedWindow(HWND hwnd) {
     );
 }
 
-/*
-void PrintStyleFlags(LONG_PTR style) {
-    if (style & WS_VISIBLE) std::cout << "WS_VISIBLE ";
-    if (style & WS_CHILD) std::cout << "WS_CHILD ";
-    if (style & WS_POPUP) std::cout << "WS_POPUP ";
-    if (style & WS_CAPTION) std::cout << "WS_CAPTION ";
-    if (style & WS_THICKFRAME) std::cout << "WS_THICKFRAME ";
-    if (style & WS_MINIMIZEBOX) std::cout << "WS_MINIMIZEBOX ";
-    if (style & WS_MAXIMIZEBOX) std::cout << "WS_MAXIMIZEBOX ";
-    if (style & WS_OVERLAPPED) std::cout << "WS_OVERLAPPED ";
-    if (style & WS_SYSMENU) std::cout << "WS_SYSMENU ";
-    if (style & WS_DISABLED) std::cout << "WS_DISABLED ";
-    if (style & WS_CLIPSIBLINGS) std::cout << "WS_CLIPSIBLINGS ";
-    if (style & WS_CLIPCHILDREN) std::cout << "WS_CLIPCHILDREN ";
-}
-
-void PrintExStyleFlags(LONG_PTR exStyle) {
-    if (exStyle & WS_EX_APPWINDOW) std::cout << "WS_EX_APPWINDOW ";
-    if (exStyle & WS_EX_TOOLWINDOW) std::cout << "WS_EX_TOOLWINDOW ";
-    if (exStyle & WS_EX_TOPMOST) std::cout << "WS_EX_TOPMOST ";
-    if (exStyle & WS_EX_LAYERED) std::cout << "WS_EX_LAYERED ";
-    if (exStyle & WS_EX_TRANSPARENT) std::cout << "WS_EX_TRANSPARENT ";
-}
-
-void PrintHierarchy(HWND hwnd) {
-    HWND owner = GetWindow(hwnd, GW_OWNER);
-    HWND root = GetAncestor(hwnd, GA_ROOT);
-    HWND rootOwner = GetAncestor(hwnd, GA_ROOTOWNER);
-    std::cout << "Active HWND: " << hwnd << "\n";
-    std::cout << "Owner: " << owner << "\n";
-    std::cout << "Root: " << root << "\n";
-    std::cout << "RootOwner: " << rootOwner << "\n";
-}
-
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lparam) {
-    auto* windows = reinterpret_cast<std::vector<WindowInfo>*>(lparam);
-
-    if (!IsWindowVisible(hwnd)) return TRUE;
-
-    if (GetWindow(hwnd, GW_OWNER) != NULL) return TRUE;
-
-    RECT r;
-    if (!GetWindowRect(hwnd, &r)) return TRUE;
-
-    char title[256];
-    GetWindowTextA(hwnd, title, sizeof(title));
-
-    if (title[0] == '\0') return TRUE;
-
-    WindowInfo info;
-    info.hwnd = hwnd;
-    info.title = title;
-    info.rect = r;
-    info.style = GetWindowLongPtr(hwnd, GWL_STYLE);
-    info.exStyle = GetWindowLongPtr(hwnd, GWL_EXSTYLE);
-
-    windows->push_back(info);
-
-    return TRUE;
-}
-*/
 void ApplyRightTop(HWND hwnd, int screenW, int screenH) {
     SetWindowPos(
         hwnd, nullptr,
@@ -190,11 +127,7 @@ LayoutState GetNextFreeSlot(LayoutState start, HWND exclude = nullptr) {
 }
 
 int main() {
-
-    LayoutState currentState = LayoutState::LeftTop;
-
     while (true) {
-        system("cls");
 
         HWND active = GetForegroundWindow();
         HWND root = GetAncestor(active, GA_ROOTOWNER);
@@ -239,23 +172,6 @@ int main() {
 
             }
 
-            /*
-            for (const auto& w : windows) {
-                if (w.hwnd == root) {
-                    std::cout << "Active Window:\n";
-                    std::cout << "Title: " << w.title << "\n";
-                    std::cout << "HWND: " << w.hwnd << "\n";
-                    std::cout << "x=" << w.rect.left
-                            << " y=" << w.rect.top
-                            << " w=" << (w.rect.right - w.rect.left)
-                            << " h=" << (w.rect.bottom - w.rect.top)
-                            << "\n\n";
-                    PrintStyleFlags(w.style);
-                    PrintExStyleFlags(w.exStyle);
-                    break;
-                }
-            }
-            */
         }   
 
         Sleep(10);
